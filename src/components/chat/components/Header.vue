2e75009aa01df04378a4f66e99d4860187ef6324
Allen Lu <allenlu@Allens-MacBook-Pro.local>
2019-09-25 19:19:56 -0500

    initial commit

---
+++ src/components/chat/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <soul-base-header fixed>
+    <template slot="left">
+      <div>
+        拒绝匹配
+        <i>∨</i>
+      </div>
+    </template>
+    <template slot="center">聊天</template>
+    <template slot="right">
+      <div>
+        <i></i>
+        关注
+      </div>
+    </template>
+  </soul-base-header>
+</template>
+
+<script>
+import SoulBaseHeader from '@/components/common/SoulBaseHeader'
+export default {
+  name: 'SoulChatHeader',
+  components: {
+    SoulBaseHeader
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/star/SoulStar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <soul-star-box></soul-star-box>
+  </div>
+</template>
+
+<script>
+import SoulStarBox from './components/Box'
+export default {
+  name: 'SoulStar',
+  components: {
+    SoulStarBox
+  }
+}
+</script>
+
+<style>
+
+</style>
